399f9a633fb55c75c4cb89f82513d3a09096ea14
MaxCharlington <maxcharlington03@yandex.ru>
2023-03-12 02:40:19 +0300

    Added test for cout uninitialized var

--- tests/test4.cpp
+++ tests/test4.cpp
@@ -11,6 +11,7 @@ int main()
     var variable = 1;
     var variable2;
     std::cout << typeof_impl(variable2) << '\n';
+    std::cout << variable2 << '\n';
     std::cout << variable << ' ' << typeof_impl(variable) << '\n';
     std::cin >> variable;
     std::cout << variable << ' ' << typeof_impl(variable) << std::endl;
